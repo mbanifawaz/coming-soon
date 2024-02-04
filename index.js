b07aa8b2c0cbbf8f2dfe8b9e4fe9c35e79bcf9c4
Ubuntu <Munes Bani Fawaz>
2024-02-04 02:37:59 +0000

    add all

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files (CSS, images, etc.) from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define a route to render the HTML template
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
